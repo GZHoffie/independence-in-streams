4b5bd4cde3312724d877f6a49f28b7b528e5dc4f
Zhenhao Gu <guzhenhao1@sjtu.edu.cn>
2021-10-28 13:50:13 +0800

    Adjust test

--- mini_project/data/generate_dataset.py
+++ mini_project/data/generate_dataset.py
@@ -93,7 +93,7 @@ class DiscreteSampleGenerator(DataGenerator):
 
 
 if __name__ == "__main__":
-    generator = DiscreteSampleGenerator(n=10, N=100000, independent=False)
+    generator = DiscreteSampleGenerator(n=1000, N=100000, independent=False)
     generator.write_file("sample")
 
     
--- mini_project/test/test_counter_matrix.py
+++ mini_project/test/test_counter_matrix.py
@@ -1,4 +1,4 @@
-from mini_project.algorithms.counter_matrix import L2Estimator
+from mini_project.algorithms.counter_matrix import L2Estimator, L1Estimator
 from mini_project.utils import check_error
 
 TEST_FILE = "sample"
@@ -7,9 +7,15 @@ def test_l2_estimator():
     """
     Test function for l2 estimator.
     """
-    estimator = L2Estimator(10, 10)
+    estimator = L2Estimator(10, 100)
     error = check_error(estimator, TEST_FILE)
+    print("multiplicative error:", error)
+
+def test_l1_estimator():
+    estimator = L1Estimator(100, 100)
+    error = check_error(estimator, TEST_FILE, metric="l1")
     print("multiplicative error:", error)
 
 if __name__ == "__main__":
     test_l2_estimator()
+    # test_l1_estimator()
