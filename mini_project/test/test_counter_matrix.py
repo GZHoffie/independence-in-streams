7b6874c599771e060de4a5ee56186c2b628a9580
Hao Zhang <zhangghaoo@gmail.com>
2021-10-31 16:49:20 +0800

    Add counter matrix test

--- mini_project/test/test_counter_matrix.py
+++ mini_project/test/test_counter_matrix.py
@@ -8,12 +8,13 @@ def test_l2_estimator():
     Test function for l2 estimator.
     """
     estimator = L2Estimator(10, 100)
-    error = check_error(estimator, TEST_FILE)
+    (res, answer, error) = check_error(estimator, TEST_FILE, metric="l2")
     print("multiplicative error:", error)
+
 
 def test_l1_estimator():
     estimator = L1Estimator(100, 100)
-    error = check_error(estimator, TEST_FILE, metric="l1")
+    (res, answer, error) = check_error(estimator, TEST_FILE, metric="l1")
     print("multiplicative error:", error)
 
 
