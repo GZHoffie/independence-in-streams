82a6f4bd3d8dc1de29625111478e2ce7862f03e7
Zhenhao Gu <guzhenhao1@sjtu.edu.cn>
2021-11-02 15:17:37 +0800

    Add display of estimator variance

--- mini_project/test/test_counter_matrix.py
+++ mini_project/test/test_counter_matrix.py
@@ -7,15 +7,15 @@ def test_l2_estimator():
     """
     Test function for l2 estimator.
     """
-    estimator = L2Estimator(10, 100)
+    estimator = L2Estimator(10, 10)
     error = check_error(estimator, TEST_FILE)
     print("multiplicative error:", error)
 
 def test_l1_estimator():
-    estimator = L1Estimator(100, 100)
+    estimator = L1Estimator(10, 100, n=1000)
     error = check_error(estimator, TEST_FILE, metric="l1")
     print("multiplicative error:", error)
 
 if __name__ == "__main__":
     test_l2_estimator()
-    # test_l1_estimator()
+    #test_l1_estimator()
